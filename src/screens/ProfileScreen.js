3012c1686c84a54fc9363090616a577f40b6a142
Mally13 <mallymutuku13@gmail.com>
2023-08-05 13:13:54 +0300

    Profile Data

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,10 +1,11 @@
-import { StyleSheet, Text, View, Image, TextInput, Pressable } from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput,KeyboardAvoidingView, Pressable } from 'react-native';
 import React, { useContext, useState, useRef } from 'react';
 import AccountHeader from '../components/AccountHeader';
 import { AuthContext } from '../contexts/AuthContext';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { ScrollView } from 'react-native-gesture-handler';
 
 const ProfileScreen = () => {
   const { isLoggedIn, userData, status, updateUser } = useContext(AuthContext);
@@ -48,182 +49,147 @@ const ProfileScreen = () => {
   return (
     <View style={[styles.container, { paddingTop: insets.top }]}>
       <AccountHeader props={{ title: 'Profile' }} />
-      <Image source={require('../assets/images/user.png')} style={styles.avatar} />
-      <Formik
-        initialValues={{
-          fullName: userData.fullName,
-        }}
-        onSubmit={handleSaveNamePress}
-        validationSchema={yup.object().shape({
-          fullName: yup.string(),
-        })}
+      <KeyboardAvoidingView
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       >
-        {({ values, handleChange, handleSubmit, isValid }) => (
-          <>
-            <View style={styles.NameInputSave}>
-              <View>
-                <TextInput
-                  ref={inputNameRef}
-                  value={values.fullName}
-                  onChangeText={handleChange('fullName')}
-                  editable={isNameEditable}
-                  style={isNameEditable ? styles.avatarTextEditable : styles.avatarText}
-                  caretColor="#00205c"
-                />
-              </View>
-              <>
-                {!isNameEditable ? (
-                  <Pressable onPress={handleEditNamePress} style={styles.editName}>
-                    <Image source={require('../assets/icons/edit.png')} style={styles.editNameIcon} />
-                  </Pressable>
-                ) : (
-                  <Pressable onPress={handleSubmit} disabled={!isValid}>
-                    <View style={!isValid ? styles.inValidSave : styles.save}>
-                      <Text style={styles.saveText}>Save</Text>
+        <ScrollView contentContainerStyle = {styles.inner} showsVerticalScrollIndicator = {false}>
+          <Image source={require('../assets/images/user.png')} style={styles.avatar} />
+            <Formik
+              initialValues={{
+                fullName: userData.fullName,
+              }}
+              onSubmit={handleSaveNamePress}
+              validationSchema={yup.object().shape({
+                fullName: yup.string(),
+              })}
+            >
+              {({ values, handleChange, handleSubmit, isValid }) => (
+                  <View style={styles.NameInputSave}>
+                    <View>
+                      <TextInput
+                        ref={inputNameRef}
+                        value={values.fullName}
+                        onChangeText={handleChange('fullName')}
+                        editable={isNameEditable}
+                        style={isNameEditable ? styles.avatarTextEditable : styles.avatarText}
+                        caretColor="#00205c"
+                      />
                     </View>
-                  </Pressable>
-                )}
-              </>
-            </View>
-          </>
-        )}
-      </Formik>
-      <Text style={styles.profileEmail}>{userData.email}</Text>
-      <View style={styles.profileDetails}>
-        <Text style={styles.profileTitle}>Phone Number</Text>
-        <Formik
-          initialValues={{
-            phoneNumber: userData.phoneNumber || 'Not set',
-          }}
-          onSubmit={handleSavePhonePress}
-          validationSchema={yup.object().shape({
-            phoneNumber: yup.string().matches(/^\d{10}$/, 'Please enter a valid 10-digit phone number'),
-          })}
-        >
-          {({ values, handleChange, handleSubmit, isValid }) => (
-            <View style={styles.profileContentContainer}>
-              <View style={styles.InputSave}>
-                <View style={styles.inputContainer}>
-                  <TextInput
-                    ref={inputPhoneRef}
-                    value={values.phoneNumber}
-                    onChangeText={handleChange('phoneNumber')}
-                    editable={isPhoneEditable}
-                    style={styles.profileContent}
-                    caretColor="#00205c"
-                  />
+                    <>
+                      {!isNameEditable ? (
+                        <Pressable onPress={handleEditNamePress} style={styles.editName}>
+                          <Image source={require('../assets/icons/edit.png')} style={styles.editNameIcon} />
+                        </Pressable>
+                      ) : (
+                        <Pressable onPress={handleSubmit} disabled={!isValid}>
+                          <View style={!isValid ? styles.inValidSaveName : styles.saveName}>
+                            <Text style={styles.saveText}>Save</Text>
+                          </View>
+                        </Pressable>
+                      )}
+                    </>
+                  </View>
+              )}
+            </Formik>
+          <Text style={styles.profileEmail}>{userData.email}</Text>
+
+          <View style={styles.profileDetails}>
+            <Text style={styles.profileTitle}>Phone Number</Text>
+            <Formik
+              initialValues={{
+                phoneNumber: userData.phoneNumber || 'Not set',
+              }}
+              onSubmit={handleSavePhonePress}
+              validationSchema={yup.object().shape({
+                phoneNumber: yup.string().matches(/^\d{10}$/, 'Please enter a valid 10-digit phone number'),
+              })}
+            >
+              {({ values, handleChange, handleSubmit, isValid }) => (
+                <KeyboardAvoidingView 
+                  style={{ flex: 1 }}
+                  behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+                >
+                  <View style={styles.profileContentContainer}>
+                    <View style={styles.InputSave}>
+                      <View style={styles.inputContainer}>
+                        <TextInput
+                          ref={inputPhoneRef}
+                          value={values.phoneNumber}
+                          onChangeText={handleChange('phoneNumber')}
+                          editable={isPhoneEditable}
+                          style={styles.profileContent}
+                          caretColor="#00205c"
+                        />
+                      </View>
+                      <>
+                        {!isPhoneEditable ? (
+                          <Pressable onPress={handleEditPhonePress}>
+                            <View style={styles.edit}>
+                              <Text style={styles.editText}>Edit</Text>
+                              <Image source={require('../assets/icons/edit.png')} style={styles.editIcon} />
+                            </View>
+                          </Pressable>
+                        ) : (
+                          <Pressable onPress={handleSubmit} disabled={!isValid}>
+                            <View style={!isValid ? styles.inValidSave : styles.save}>
+                              <Text style={styles.saveText}>Save</Text>
+                            </View>
+                          </Pressable>
+                        )}
+                      </>
+                    </View>
+                  </View>
+                </KeyboardAvoidingView>
+              )}
+            </Formik>
+          </View>
+          <View style={styles.profileDetails}>
+            <Text style={styles.profileTitle}>Delivery Address</Text>
+            <Formik
+              initialValues={{
+                deliveryAddress: userData.deliveryAddress || 'Not set',
+              }}
+              onSubmit={handleSaveAddressPress}
+              validationSchema={yup.object().shape({
+                deliveryAddress: yup.string(),
+              })}
+            >
+              {({ values, handleChange, handleSubmit, isValid }) => (
+                <View style={styles.profileContentContainer}>
+                  <View style={styles.InputSave}>
+                    <View style={styles.inputContainer}>
+                      <TextInput
+                        ref={inputAddressRef}
+                        value={values.deliveryAddress}
+                        onChangeText={handleChange('deliveryAddress')}
+                        editable={isAddressEditable}
+                        style={styles.profileContent}
+                        caretColor="#00205c"
+                      />
+                    </View>
+                    <>
+                      {!isAddressEditable ? (
+                        <Pressable onPress={handleEditAddressPress}>
+                          <View style={styles.edit}>
+                            <Text style={styles.editText}>Edit</Text>
+                            <Image source={require('../assets/icons/edit.png')} style={styles.editIcon} />
+                          </View>
+                        </Pressable>
+                      ) : (
+                        <Pressable onPress={handleSubmit} disabled={!isValid}>
+                          <View style={!isValid ? styles.inValidSave : styles.save}>
+                            <Text style={styles.saveText}>Save</Text>
+                          </View>
+                        </Pressable>
+                      )}
+                    </>
+                  </View>
                 </View>
-                <>
-                  {!isPhoneEditable ? (
-                    <Pressable onPress={handleEditPhonePress}>
-                      <View style={styles.edit}>
-                        <Text style={styles.editText}>Edit</Text>
-                        <Image source={require('../assets/icons/edit.png')} style={styles.editIcon} />
-                      </View>
-                    </Pressable>
-                  ) : (
-                    <Pressable onPress={handleSubmit} disabled={!isValid}>
-                      <View style={!isValid ? styles.inValidSave : styles.save}>
-                        <Text style={styles.saveText}>Save</Text>
-                      </View>
-                    </Pressable>
-                  )}
-                </>
-              </View>
-            </View>
-          )}
-        </Formik>
-      </View>
-      <View style={styles.profileDetails}>
-        <Text style={styles.profileTitle}>Delivery Address</Text>
-        <Formik
-          initialValues={{
-            userAddress: userData.userAddress || 'Not set',
-          }}
-          onSubmit={handleSaveAddressPress}
-          validationSchema={yup.object().shape({
-            userAddress: yup.string(),
-          })}
-        >
-          {({ values, handleChange, handleSubmit, isValid }) => (
-            <View style={styles.profileContentContainer}>
-              <View style={styles.InputSave}>
-                <View style={styles.inputContainer}>
-                  <TextInput
-                    ref={inputAddressRef}
-                    value={values.userAddress}
-                    onChangeText={handleChange('userAddress')}
-                    editable={isAddressEditable}
-                    style={styles.profileContent}
-                    caretColor="#00205c"
-                  />
-                </View>
-                <>
-                  {!isAddressEditable ? (
-                    <Pressable onPress={handleEditAddressPress}>
-                      <View style={styles.edit}>
-                        <Text style={styles.editText}>Edit</Text>
-                        <Image source={require('../assets/icons/edit.png')} style={styles.editIcon} />
-                      </View>
-                    </Pressable>
-                  ) : (
-                    <Pressable onPress={handleSubmit} disabled={!isValid}>
-                      <View style={!isValid ? styles.inValidSave : styles.save}>
-                        <Text style={styles.saveText}>Save</Text>
-                      </View>
-                    </Pressable>
-                  )}
-                </>
-              </View>
-            </View>
-          )}
-        </Formik>
-      </View>
-      {/* <View style={styles.profileDetails}>
-        <Text style={styles.profileTitle}>Payment Details</Text>
-        <Formik
-          initialValues={{
-            phoneNumber: userData.phoneNumber || 'Not set',
-          }}
-          onSubmit={handleSavePhonePress}
-          validationSchema={yup.object().shape({
-            phoneNumber: yup.string().matches(/^\d{10}$/, 'Please enter a valid 10-digit phone number'),
-          })}
-        >
-          {({ values, handleChange, handleSubmit, isValid }) => (
-            <View style={styles.profileContentContainer}>
-              <View style={styles.InputSave}>
-                <View style={styles.inputContainer}>
-                  <TextInput
-                    ref={inputPhoneRef}
-                    value={values.phoneNumber}
-                    onChangeText={handleChange('phoneNumber')}
-                    editable={isPhoneEditable}
-                    style={styles.profileContent}
-                    caretColor="#00205c"
-                  />
-                </View>
-                <>
-                  {!isPhoneEditable ? (
-                    <Pressable onPress={handleEditPhonePress}>
-                      <View style={styles.edit}>
-                        <Text style={styles.editText}>Edit</Text>
-                        <Image source={require('../assets/icons/edit.png')} style={styles.editIcon} />
-                      </View>
-                    </Pressable>
-                  ) : (
-                    <Pressable onPress={handleSubmit} disabled={!isValid}>
-                      <View style={!isValid ? styles.inValidSave : styles.save}>
-                        <Text style={styles.saveText}>Save</Text>
-                      </View>
-                    </Pressable>
-                  )}
-                </>
-              </View>
-            </View>
-          )}
-        </Formik>
-      </View> */}
+              )}
+            </Formik>
+          </View>
+        </ScrollView>
+      </KeyboardAvoidingView>
     </View>
   );
 };
@@ -235,6 +201,9 @@ const styles = StyleSheet.create({
     height: '100%',
     width: '100%',
     paddingHorizontal: 24,
+  },
+  inner:{
+    paddingVertical: 8,
   },
   avatar: {
     marginTop: 16,
@@ -252,15 +221,20 @@ const styles = StyleSheet.create({
     marginVertical: 18,
   },
   avatarTextEditable: {
-    padding: 4,
+    paddingVertical: 4,
+    paddingHorizontal:16,
     borderWidth: 1,
-    borderColor: 'rgba(0, 32, 92, 0.6)',
+    borderRadius:8,
+    flex:0.8,
+    borderColor: 'rgba(0, 32, 92, 0.3)',
     fontFamily: 'Poppins_600SemiBold',
     fontSize: 16,
     lineHeight: 42,
     color: '#00205C',
     textAlign: 'center',
     marginVertical: 18,
+    marginRight:16,
+    backgroundColor:"#fff"
   },
   profileEmail: {
     fontFamily: 'Poppins_400Regular',
@@ -314,7 +288,9 @@ const styles = StyleSheet.create({
   NameInputSave: {
     flexDirection: 'row',
     justifyContent: 'center',
-    paddingVertical: 4,
+    alignItems: 'center',
+    width: '100%',
+    borderColor: 'rgba(0, 32, 92, 0.6)',
   },
   edit: {
     borderColor: 'rgba(0, 32, 92, 0.3)',
@@ -373,6 +349,30 @@ const styles = StyleSheet.create({
     backgroundColor: '#F8E2B4',
   },
   save: {
+    borderColor: '#E69f14',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    height: 28,
+    width: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    flexDirection: 'row',
+    backgroundColor: '#E69f14',
+  },
+  inValidSaveName: {
+    borderColor: '#F8E2B4',
+    borderWidth: 1,
+    paddingHorizontal: 8,
+    height: 28,
+    width: 56,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    flexDirection: 'row',
+    backgroundColor: '#F8E2B4',
+  },
+  saveName: {
     borderColor: '#E69f14',
     borderWidth: 1,
     paddingHorizontal: 8,
